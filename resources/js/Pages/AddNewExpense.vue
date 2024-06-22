f1a58667a01ea543b562bc59774f4b84ab11bce0
Lawrence <e0176571@u.nus.edu>
2024-06-23 02:26:32 +0800

    optimize: add placeholder if user does not have any groups

--- resources/js/Pages/AddNewExpense.vue
+++ resources/js/Pages/AddNewExpense.vue
@@ -2,18 +2,27 @@
 import ExpenseForm from "@/Components/Expense/ExpenseForm.vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import "v-calendar/style.css";
+import { FaceSmileIcon } from "@heroicons/vue/24/outline/index.js";
 
 // #region Configs
 const props = defineProps({
     groups: Array,
     categories: Array,
     currencies: Array,
-    auth: Object,
+    auth: Object
 });
 </script>
 
 <template>
     <AppLayout title="Add Expense">
-        <ExpenseForm :groups :categories :currencies :auth :isEdit="false" />
+        <template v-if="groups.length">
+            <ExpenseForm :auth :categories :currencies :groups :isEdit="false" />
+        </template>
+        <div v-else
+             class="flex flex-col items-center px-4 pt-10 text-center text-gray-300 sm:px-6 lg:px-8 dark:text-gray-600"
+        >
+            <FaceSmileIcon class="h-12 w-12" />
+            <span>Join or create a group to get started</span>
+        </div>
     </AppLayout>
 </template>
--- resources/js/Pages/SettleUp.vue
+++ resources/js/Pages/SettleUp.vue
@@ -2,6 +2,7 @@
 import SettleUpForm from "@/Components/Expense/SettleUpForm.vue";
 import AppLayout from "@/Layouts/AppLayout.vue";
 import "v-calendar/style.css";
+import { FaceSmileIcon } from "@heroicons/vue/24/outline/index.js";
 
 // #region Configs
 const props = defineProps({
@@ -9,12 +10,20 @@ const props = defineProps({
     categories: Array,
     currencies: Array,
     auth: Object,
-    groupBalance: Object,
+    groupBalance: Object
 });
 </script>
 
 <template>
     <AppLayout title="Settle Up">
-        <SettleUpForm :groups :categories :currencies :auth :groupBalance />
+        <template v-if="groups.length">
+            <SettleUpForm :auth :categories :currencies :groupBalance :groups />
+        </template>
+        <div v-else
+             class="flex flex-col items-center px-4 pt-10 text-center text-gray-300 sm:px-6 lg:px-8 dark:text-gray-600"
+        >
+            <FaceSmileIcon class="h-12 w-12" />
+            <span>Join or create a group to get started</span>
+        </div>
     </AppLayout>
 </template>
