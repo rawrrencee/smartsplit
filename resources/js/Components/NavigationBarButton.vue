f1f8e33a7b8223b84fba858b38bfc37498e325f8
Lawrence <e0176571@u.nus.edu>
2024-06-23 02:12:04 +0800

    fix: update colour of back button when in dark mode

--- resources/js/Components/NavigationBarButton.vue
+++ resources/js/Components/NavigationBarButton.vue
@@ -10,7 +10,7 @@ defineEmits(["onClick"]);
 </script>
 
 <template>
-    <span class="loading loading-spinner py-6" v-if="isLoading"></span>
+    <span class="loading loading-spinner py-6 dark:bg-gray-200" v-if="isLoading"></span>
     <button v-else type="button" class="btn btn-link px-0 text-gray-600 dark:text-gray-200" @click="$emit('onClick')">
         <component :is="icon" class="h-5 w-5 dark:text-gray-200" />
     </button>
